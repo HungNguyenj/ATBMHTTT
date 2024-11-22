13ebb3b2e5244cb1685b3c8393757d1c69b7dd49
HungNg <21130367@st.hcmuaf.edu.vn>
2024-11-22 17:31:28 +0700

    digital sign

---
+++ shoes-ecommerce-servlet-main/src/main/java/com/ltweb_servlet_ecommerce/model/DigitalSign.java
@@ -0,0 +1,9 @@
+package com.ltweb_servlet_ecommerce.model;
+
+public class DigitalSign {
+
+    public void genkey(){}
+    public void getKey(){}
+    public void sign(){}
+    public void verify(){}
+}
